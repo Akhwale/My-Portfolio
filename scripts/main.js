bf871d0584d2983891dadd9149be98916b302719
Akhwale <educhris99@gmail.com>
2023-02-09 23:30:52 +0300

    Homepage and about 90% done

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,33 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
 
+
+
+
+
+
+
 document.querySelector("html").addEventListener("click", function () {
     alert("Ouch! Stop poking me!");
   });
+
+      var tablinks = document.getElementsByClassName('tab-links');
+          var tabcontents = document.getElementsByClassName('tab-contents');
+
+
+
+function navigate(tabname){
+  for(tablink of tablinks){
+    tablink.classList.remove("active-link");
+  }
+  for(tabcontent of tabcontents){
+    tabcontent.classList.remove("active-tab");
+
+  }
+
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+
+}
+
+  
