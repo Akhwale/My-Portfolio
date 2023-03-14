8b3ada6f135589c83aa93d85c016afa078be4f97
Akhwale <educhris99@gmail.com>
2023-03-14 22:05:04 +0300

    Commit with responsive screens

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,66 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
 
 
+// Tab Navigation
 
-
-
-
-
-document.querySelector("html").addEventListener("click", function () {
-    alert("Ouch! Stop poking me!");
-  });
-
-      var tablinks = document.getElementsByClassName('tab-links');
-          var tabcontents = document.getElementsByClassName('tab-contents');
-
+var tablinks = document.getElementsByClassName('tab-links');
+var tabcontents = document.getElementsByClassName('tab-contents');
 
 
 function navigate(tabname){
-  for(tablink of tablinks){
-    tablink.classList.remove("active-link");
-  }
-  for(tabcontent of tabcontents){
-    tabcontent.classList.remove("active-tab");
-
-  }
-
-  event.currentTarget.classList.add("active-link");
-  document.getElementById(tabname).classList.add("active-tab");
+for(tablink of tablinks){
+tablink.classList.remove("active-link");
+}
+for(tabcontent of tabcontents){
+tabcontent.classList.remove("active-tab");
+}
+event.currentTarget.classList.add("active-link");
+document.getElementById(tabname).classList.add("active-tab");
 
 }
 
-  
+// Side Menu Script
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openMenu(){
+  sidemenu.style.right="0";
+}
+function closeMenu(){
+  sidemenu.style.right="-300px";
+}
+
+
+// web-development gallery
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+let i;
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("demo");
+let captionText = document.getElementById("caption");
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+slides[i].style.display = "none";
+}
+for (i = 0; i < dots.length; i++) {
+dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
+captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
